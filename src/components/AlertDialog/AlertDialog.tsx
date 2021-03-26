cfffa0c668811c2f232787b4e8e586f939973b2b
quanghuy30101999 <huytran.301099@gmail.com>
2021-03-26 10:26:57 +0700

    Convert JS to TS

---
+++ src/components/AlertDialog/AlertDialog.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function AlertDialog(props: any) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onDelete = (id: number) => {
+    props.onDelete(id);
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <i
+        className="remove mdi mdi-close-circle-outline"
+        onClick={handleClickOpen}
+      />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Do you really want to delete?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={() => onDelete(props.todo.id)} color="primary">
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
